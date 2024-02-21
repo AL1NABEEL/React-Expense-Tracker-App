19465caf9e48e92157be62c15206a4d3226aaad8
Ali Nabeel <alinabeel.1980@gmail.com>
2024-02-21 21:12:32 +0300

    expense tracker page form

--- src/pages/expense-tracker/index.jsx
+++ src/pages/expense-tracker/index.jsx
@@ -1,5 +1,33 @@
 export const ExpenseTracker= ()=> {
-    return <div>
-        hello world expense tracker 
+    return <div className="expense-tracker">
+        <div className="container">
+            <h1>expense tracker</h1>
+            <div className="balance">
+                <h3>your balance</h3>
+                <h2>$0.00</h2>
+
+            </div>
+            <div className="summary">
+                <div className="income">
+                    <h4>income</h4>
+                    <p>$0.00</p>
+                </div>
+                <div className="expenses">
+                <h4>expenses</h4>
+                    <p>$0.00</p>
+                </div>
+
+            </div>
+            <form action="add-transaction">
+                <input type="text" placeholder="Description" required />
+                <input type="number" placeholder="amount" required />
+                <input type="radio" id="expense" value='expense' />
+                <label htmlFor="expense">expense</label>
+                <input type="radio" id="income" value='income' />
+                <label htmlFor="income">income</label>
+
+                <button type="submit">add transaction</button>
+            </form>
+        </div>
     </div>
 };
