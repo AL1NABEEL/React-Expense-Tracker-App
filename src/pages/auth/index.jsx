885a20a77efc584cb4dc36773a203a0fdb95911c
Ali Nabeel <alinabeel.1980@gmail.com>
2024-02-21 20:40:48 +0300

    creating login with google button

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,3 +1,15 @@
+import { auth, provider } from "../../config/firebase-config";
+import { signInWithPopup } from "firebase/auth";
+
 export const Auth = ()=>{
-    return <div>hello world</div>
+
+const signInWithGoogle = async()=> {
+const results = await signInWithPopup(auth, provider)
+console.log(results);
+}
+
+    return <div className="login-page">
+<p>sign in with google to continue</p>
+<button className="login-with-google-btn" onClick={signInWithGoogle}> sign in with google</button>
+    </div>
 }
